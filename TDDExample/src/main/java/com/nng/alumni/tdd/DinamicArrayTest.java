baafd1fd5104008bd6c53c44f157f7a52d7146cc
karmantibi <karman.tibor@mailbox.hu>
2016-11-17 22:16:48 +0100

    add test class

---
+++ TDDExample/src/main/java/com/nng/alumni/tdd/DinamicArrayTest.java
@@ -0,0 +1,15 @@
+package com.nng.alumni.tdd;
+
+public class DinamicArrayTest {
+	
+	/**
+	 * Készíts egy DynamicArray nevû generikus osztályt.
+	 * Az osztály a következõket tudja:
+	 * 		meg tudja mondani megáról, hány tetszõleges elemet tartalmaz.
+	 * 		hozzá tudunk adni új elemet
+	 * 		le tudunk kérni tetszõleges indexû elemet
+	 * 		el tudunk venni elemet belõle tetszõleges indexbõl.
+	 * 
+	 */
+
+}
